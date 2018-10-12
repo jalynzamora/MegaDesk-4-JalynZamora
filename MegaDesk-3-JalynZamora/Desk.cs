b231edff8d43d74f976756f796fb33d94ae127ae
Jalyn Zamora Ortiz <jalynortiz@yahoo.com>
2018-10-12 14:01:50 -0600

    first commit

---
+++ MegaDesk-3-JalynZamora/Desk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_3_JalynZamora
+{
+    public class Desk
+    {
+        public int Width { get; set; }
+
+        public int Depth { get; set; }
+
+        
+    }
+}
---
+++ MegaDesk-3-JalynZamora/MainMenu.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MegaDesk_3_JalynZamora
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddQuoteButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddQuoteButton
+            // 
+            this.AddQuoteButton.Location = new System.Drawing.Point(71, 80);
+            this.AddQuoteButton.Name = "AddQuoteButton";
+            this.AddQuoteButton.Size = new System.Drawing.Size(175, 45);
+            this.AddQuoteButton.TabIndex = 0;
+            this.AddQuoteButton.Text = "Add New Quote";
+            this.AddQuoteButton.UseVisualStyleBackColor = true;
+            this.AddQuoteButton.Click += new System.EventHandler(this.AddQuoteButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "View Quotes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ViewAllQuotesButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Search Quotes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SearchQuotesButton_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(71, 275);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AddQuoteButton);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddQuoteButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
